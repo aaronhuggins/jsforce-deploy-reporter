76cf727c92682d98aefbd840f595d3976eb6b03d
Aaron Huggins <ahuggins@nethealthshops.com>
2020-01-24 12:32:17 -0600

    Removed deprecated lines from test.js

--- test/test.js
+++ test/test.js
@@ -1,6 +1,5 @@
 const { JSforceReporter } = require('../index')
 const testFile = require('./deploy-result.json')
-const processor = require('jest-stare')
 
 const main = async function main () {
   const reporter = new JSforceReporter(testFile.details, {
@@ -11,10 +10,6 @@ const main = async function main () {
   })
   
   await reporter.report()
-
-  //const tests = require("../coverage/jest-results.json")
-
-  //processor(tests, { log: false, resultDir: './coverage/test-report' })
 }
 
 main()
