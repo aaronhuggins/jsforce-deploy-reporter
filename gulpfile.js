0e82ad52222bb0faf8aac60ac694b477a8410e59
Aaron Huggins <ahuggins@nethealthshops.com>
2020-01-28 16:37:01 -0600

    Implement unit tests and code coverage on project

--- gulpfile.js
+++ gulpfile.js
@@ -1,59 +1,9 @@
 const prettier = require('prettier-standard')
+const jestCli = require('jest-cli')
 const gulp = require('gulp')
-const zip = require('gulp-zip')
-const { JSforceReporter, jsforceGulpReporter } = require('./index')
-const { deploy } = require('@nhs-llc/gulp-jsforce-deploy')
 
 gulp.task('test', async () => {
-  return new Promise((resolve, reject) => {
-    gulp
-      .src('./test/**', { base: './' })
-      .pipe(zip('pkg.zip'))
-      .pipe(
-        deploy({
-          username: process.env['sf.username'],
-          password: process.env['sf.password'],
-          loginUrl: process.env['sf.serverurl'],
-          checkOnly: true,
-          checkOnlyNoFail: true,
-          verbose: true,
-          resultOnly: true
-        })
-      )
-      .pipe(gulp.dest('./'))
-      .on('finish', resolve)
-      .on('error', reject)
-  })
-})
-
-gulp.task('coverage', async () => {
-  const { completedDate, details } = require('./test/deploy-result.json')
-  const reporter = new JSforceReporter(details, {
-    reporters: ['cobertura', 'junitonly', 'lcov', 'jeststare'],
-    detectExecutableLines: true,
-    packageRoot: './test',
-    outputRoot: './coverage',
-    completedDate
-  })
-
-  await reporter.report()
-})
-
-gulp.task('gulp-coverage', async () => {
-  return new Promise((resolve, reject) => {
-    gulp
-      .src('./test/deploy-result.json', { base: './test' })
-      .pipe(
-        jsforceGulpReporter({
-          reporters: ['cobertura', 'junitonly', 'lcov', 'jeststare'],
-          detectExecutableLines: true,
-          packageRoot: './test'
-        })
-      )
-      .pipe(gulp.dest('./coverage/gulp'))
-      .on('finish', resolve)
-      .on('error', reject)
-  })
+  await jestCli.run()
 })
 
 gulp.task('format', async () => {
---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  reporters: [
+    'default',
+    [
+      'jest-stare',
+      {
+        log: false,
+        resultDir: './coverage/jest-stare/',
+        coverageLink: '../lcov-report/index.html'
+      }
+    ]
+  ],
+  collectCoverage: true,
+  coverageDirectory: './coverage',
+  coverageReporters: ['lcov'],
+  testRegex: '(/test/.*(spec|test|[Tt]est|[Ss]uite))\\.[tj]s(x){0,1}'
+}
