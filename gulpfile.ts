e7141677b66d4ae5b860e895878f623135da57c2
Aaron Huggins <ahuggins@nethealthshops.com>
2021-02-20 14:53:17 -0600

    Add more tasks and update packages and npm scripts

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,3 +1,4 @@
+import * as fs from 'fs-extra'
 import * as shell from 'gulp-shell'
 
 export async function test (): Promise<void> {
@@ -8,6 +9,22 @@ export async function coverage (): Promise<void> {
   await shell.task('nyc mocha')()
 }
 
+export async function lint (): Promise<void> {
+  await shell.task('ts-standard')()
+}
+
 export async function format (): Promise<void> {
   await shell.task('ts-standard --fix')()
+}
+
+export async function tsc (): Promise<void> {
+  await shell.task('tsc')()
+}
+
+export async function compile (): Promise<void> {
+  await shell.task('tsc --sourceMap false')()
+}
+
+export async function cleanup (): Promise<void> {
+  fs.rmdirSync('./dist', { recursive: true })
 }
